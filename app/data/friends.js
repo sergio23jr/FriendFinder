9efca938862f25c1c00aee70e6d36182b7ac7945
Sergio Jr <sergio23jr@gmail.com>
2018-12-07 21:58:48 -0600

    last commit

---
+++ app/data/friends.js
@@ -0,0 +1,148 @@
+var friendsArray = [
+    {
+        name: 'Scooter',
+        photo: 'https://randomuser.me/api/portraits/men/13.jpg',
+        scores: [
+            '3',
+            '2',
+            '1',
+            '5',
+            '1',
+            '1',
+            '2',
+            '2',
+            '1',
+            '4'
+        ]
+    },
+    {
+        name: 'Silvia',
+        photo: 'https://randomuser.me/api/portraits/women/25.jpg',
+        scores: [
+            '2',
+            '4',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '2',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Spencer',
+        photo: 'https://randomuser.me/api/portraits/men/90.jpg',
+        scores: [
+            '5',
+            '2',
+            '5',
+            '1',
+            '1',
+            '5',
+            '4',
+            '2',
+            '4',
+            '1'
+        ]
+    },
+    {
+        name: 'Matt',
+        photo: 'https://randomuser.me/api/portraits/men/53.jpg',
+        scores: [
+            '5',
+            '1',
+            '2',
+            '1',
+            '3',
+            '4',
+            '1',
+            '3',
+            '3',
+            '4'
+        ]
+    },
+    {
+        name: 'Ray',
+        photo: 'https://randomuser.me/api/portraits/men/83.jpg',
+        scores: [
+            '1',
+            '1',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '1'
+        ]
+    },
+    {
+        name: 'Camilia',
+        photo: 'https://randomuser.me/api/portraits/women/47.jpg',
+        scores: [
+            '1',
+            '5',
+            '5',
+            '2',
+            '3',
+            '2',
+            '4',
+            '1',
+            '2',
+            '5'
+        ]
+    },
+    {
+        name: 'Beyonce Knowles',
+        photo: 'https://randomuser.me/api/portraits/women/34.jpg',
+        scores: [
+            '5',
+            '4',
+            '4',
+            '2',
+            '4',
+            '4',
+            '3',
+            '2',
+            '3',
+            '3'
+        ]
+    },
+    {
+        name: 'Chavez',
+        photo: 'https://randomuser.me/api/portraits/men/19.jpg',
+        scores: [
+            '5',
+            '2',
+            '3',
+            '1',
+            '5',
+            '1',
+            '3',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Grinch',
+        photo: 'https://www.google.com/imgres?imgurl=http%3A%2F%2Fwww.bochcenter.org%2F~%2Fmedia%2F2018-shows%2Fgrinchbanner1200x675.jpg%3Fmw%3D1200%26mh%3D845&imgrefurl=http%3A%2F%2Fwww.bochcenter.org%2Fbuy%2Fshow-listing%2Fhow-the-grinch-stole-christmas&docid=hiiNRA_DMvc3WM&tbnid=_dN0H6hoQIhgLM%3A&vet=10ahUKEwj6hPGZqo_fAhVl6IMKHZ02D28QMwhyKAkwCQ..i&w=1200&h=675&bih=635&biw=1366&q=the%20grinch&ved=0ahUKEwj6hPGZqo_fAhVl6IMKHZ02D28QMwhyKAkwCQ&iact=mrc&uact=8',
+        scores: [
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1'
+        ]
+    }
+];
+
+module.exports = friendsArray;
